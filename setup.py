54e9c487d4137f2759560ae318a8b445be98dbf4
Stijn Van Campenhout <stijn.vancampenhout@gmail.com>
2016-11-03 15:34:31 +0000

    Initial setup definition

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from setuptools import setup
+import re
+ 
+ 
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('denonavr/denon.py').read(),
+    re.M
+    ).group(1)
+ 
+ 
+with open("README.md", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+ 
+ 
+setup(
+    name="denonavr",
+    version=version,
+    packages = ["denonavr"],
+    description="Denon AVR controller",
+    long_description=long_descr,
+   
+    entry_points = {
+        "console_scripts": ['denonavr-cli = denonavr.cli:main']
+    },
+    install_requires= [
+    
+        "requests==2.11.1"
+        
+    ]
+)
