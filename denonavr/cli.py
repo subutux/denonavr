8e5f290eebc84f1322627a458843975214421b56
Stijn Van Campenhout <stijn.vancampenhout@gmail.com>
2016-11-03 15:23:19 +0000

    Added a basic CLI application for executing Telnet commands

---
+++ denonavr/cli.py
@@ -0,0 +1,17 @@
+import .denon as Denon
+import argparse
+
+def main():
+    
+	p = argparse.ArgumentParser(description="Remote Denon controller")
+	p.add_argument('-H','--host',help="IP or host of the denon system to connect to",required=True)
+	p.add_argument('-z','--zone',help="The zone to use (default: MAINZONE",default="MAINZONE")
+	p.add_argument('CMD',help="A telnet command to execute")
+	args = p.parse_args();
+	
+	MAINZONE=Denon.Connect(args.host,args.zone)
+	MAINZONE.exec(args.CMD)
+
+	
+	
+
