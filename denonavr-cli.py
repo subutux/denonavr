34892d4a412cab0a8a6bb19fc3fdfb7d4ee4a388
Stijn Van Campenhout <stijn.vancampenhout@gmail.com>
2016-11-03 15:35:22 +0000

    added a simple wrapper for running denonavr.cli locally

---
+++ denonavr-cli.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+ 
+ 
+"""Convenience wrapper for running denonavr.cli directly from source tree."""
+ 
+ 
+from denonavr.cli import main
+ 
+ 
+if __name__ == '__main__':
+    main()
